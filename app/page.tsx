cad04489b08b5cd06ce3fcb67fd6eb43ba997021
basyayev122 <larouefc@gmail.com>
2026-04-07 12:47:46 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
-export default function Home() {
+import { supabase } from "../lib/supabase";
+
+export const revalidate = 60;
+
+async function getSongs() {
+  const { data, error } = await supabase
+    .from("songs")
+    .select("id, slug, title, artist, category")
+    .order("created_at", { ascending: false });
+
+  if (error) return [];
+  return data ?? [];
+}
+
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <main style={{ padding: 24 }}>
-      <h1>Basyayev Chord</h1>
-      <p>Website aktif.</p>
+      <h1>Daftar Lagu (DB)</h1>
+      <ul>
+        {songs.map((s: any) => (
+          <li key={s.id}>
+            {s.title} - {s.artist} ({s.category})
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
